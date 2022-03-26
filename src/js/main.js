3ab3a6efc7ee6e0a1b2f5d6ddc75fcf4fcdc3238
hyper-design <hyper-design@keramin.int>
2022-03-26 16:30:10 +0300

    save

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,20 @@
-const swiper = new Swiper('.main-swiper', {
+const mainSwiper = new Swiper('.main-swiper', {
   loop: true,
   effect: 'fade',
   pagination: {
     el: '.main-slider-pagination',
   }
+});
+
+const materialsSwiper = new Swiper('.materials-swiper', {
+  loop: true,
+  slidesPerView: 4,
+  pagination: {
+    el: '.materials-slider-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.materials-slider-next',
+    prevEl: '.materials-slider-prev',
+  },
 });
